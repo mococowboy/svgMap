b007542ab0763a584e1afc281ff21b126a91484d
macmini1 <mtolson@live.com>
2019-03-01 21:55:39 -0500

    Updated the php dialect used to 7 and used a different database driver

--- check_sp.php
+++ check_sp.php
@@ -2,25 +2,36 @@
 header('Content-Type: application/json');
 $year = $_GET['year'];
 $month = $_GET['month'];
+
 if (! preg_match("/^([0-9][0-9][0-9][0-9])$/", $year)) {
-echo 'Year is malformed.';
-exit();
+    echo 'Year is malformed.';
+    exit();
 }
 if (! preg_match("/^(M[0|1][0-9])$/", $month)) {
-echo 'Month is malformed.';
-exit();
+    echo 'Month is malformed.';
+    exit();
 }
-require 'connect.php';
-$query = "call unem_month('".$year."', '".$month."');";
-$result = mysql_query($query);
-if (!$result) {
-	echo 'Could not run query: ' . mysql_error();
-	exit;
+
+$conn = new mysqli("localhost", "username", "password", "database");
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
-while( $row = mysql_fetch_assoc($result) ) {
-	settype($row['rate'],"float");
-	$arr[$row['state']] = $row['rate'];
+
+$sql = "call unem_month('".$year."', '".$month."');";
+
+$result = $conn -> query($sql);
+
+$data = array();
+
+if ($result -> num_rows > 0) {
+	while ($row = $result->fetch_assoc()) {
+		$data[] = $row;
+	}
+	echo json_encode($data);
 }
-echo json_encode($arr);
-mysql_close($link);
+else {
+	echo "0 results";
+}
+$conn->close();
 ?>
